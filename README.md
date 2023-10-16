d0ca63ebadb76fba3ce50d4078cc8c15a28ea332
eastmarch <29557084+eastmarch@users.noreply.github.com>
2023-10-16 03:25:37 -0300

    feat: open task viewer

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 ## Volumouse
 
 Control system settings using the cursor + mousewheel:
- * Top-left corner: turn the volume up and down.
- * Top-right corner: switch between virtual desktops.
+ * Top-left corner: turn the volume up and down
+ * Top-right corner: switch between virtual desktops
+
+Mouse middle click:
+ * Top-left corner: open task viewer
 
 ### Hotkeys
- * `CTRL+ALT+SHIFT+F12`: quit program.
- * `CTRL+ALT+F12`: force update corner coordinates.
+ * `ALT+SHIFT+F12`: quit program
+ * `CTRL+ALT+F12`: force update corner coordinates
 
 ### Details
 This program only works on the main monitor. Any corner besides top-left needs to be calculated in relation to the screen resolution. To address that without adding more event listeners for resolution and DPI changes. The "force update" hotkey was made available.
--- hotcorner.c
+++ hotcorner.c
@@ -1,24 +1,16 @@
 #define WIN32_LEAN_AND_MEAN
+#define CORNER_SIZE 20
 #include <stdlib.h>
 #include <windows.h>
 
 #pragma comment(lib, "USER32")
 #pragma comment(linker, "/SUBSYSTEM:WINDOWS")
 
-#define KEYDOWN(k) ((k) & 0x80)
-
-// If mouse enters this rectangle, activate top-left hotcorner function.
-static const RECT kTopLeftHotCorner = {
-    .left   = -20,
-	.top    = -20,
-	.right  = +20,
-    .bottom = +20,
-};
-
-// Screen resolution is needed for any other corner besides top-left.
+// If mouse enters the rectangle, activate hotcorner function
+static RECT kTopLeftHotCorner = {};
 static RECT kTopRightHotCorner = {};
 
-// Inputs to inject when corner activated.
+// Inputs to inject when corner activated
 static const INPUT kVolumeUpInput[] = {
     { INPUT_KEYBOARD, .ki = { VK_VOLUME_UP, .dwFlags = 0 }},
     { INPUT_KEYBOARD, .ki = { VK_VOLUME_UP, .dwFlags = KEYEVENTF_KEYUP }},
@@ -47,51 +39,52 @@ static const INPUT kDesktopRightInput[] = {
     { INPUT_KEYBOARD, .ki = { VK_CONTROL, .dwFlags = KEYEVENTF_KEYUP }},
 };
 
-// Update corner coordinates with the hotkey CTRL+ALT+F12.
-// Useful when resolution or DPI changes.
-// Quit program with CTRL+ALT+SHIFT+F12.
+static const INPUT kTaskViewInput[] = {
+    { INPUT_KEYBOARD, .ki = { VK_LWIN, .dwFlags = 0 }},
+    { INPUT_KEYBOARD, .ki = { VK_TAB,  .dwFlags = 0 }},
+    { INPUT_KEYBOARD, .ki = { VK_TAB,  .dwFlags = KEYEVENTF_KEYUP }},
+    { INPUT_KEYBOARD, .ki = { VK_LWIN, .dwFlags = KEYEVENTF_KEYUP }},
+};
+
+// Update corner coordinates with the hotkey CTRL+ALT+F12
+// Useful when resolution or DPI changes
+// Quit program with ALT+SHIFT+F12
 static const DWORD kHotKeyModUpdate = MOD_CONTROL | MOD_ALT;
-static const DWORD kHotKeyModQuit = MOD_CONTROL | MOD_ALT | MOD_SHIFT;
+static const DWORD kHotKeyModQuit = MOD_ALT | MOD_SHIFT;
 static const DWORD kHotKey = VK_F12;
 
-// Mouse event handler
-static LRESULT CALLBACK MouseHookCallback(int nCode, WPARAM wParam, LPARAM lParam) {
+static bool modifierKeysPressedDown() {
+    return GetAsyncKeyState(VK_SHIFT) 
+        || GetAsyncKeyState(VK_CONTROL) 
+        || GetAsyncKeyState(VK_MENU) 
+        || GetAsyncKeyState(VK_LBUTTON) 
+        || GetAsyncKeyState(VK_RBUTTON);
+}
+
+static LRESULT HandleMiddleButtonUpEvent(int nCode, WPARAM wParam, LPARAM lParam) {
     MSLLHOOKSTRUCT *evt = (MSLLHOOKSTRUCT *) lParam;
-    BYTE KeyState[256];
 
-    // If there was no mouse wheel rotation, do nothing.
-    if (wParam != WM_MOUSEWHEEL)
-        goto finish;
-
-    // Check if a mouse button is pressed
-    if (GetKeyState(VK_LBUTTON) < 0 || GetKeyState(VK_RBUTTON) < 0) {
-        goto finish;
+    if (PtInRect(&kTopLeftHotCorner, evt->pt) && !modifierKeysPressedDown()) {
+        SendInput(_countof(kTaskViewInput), kTaskViewInput, sizeof(INPUT));
     }
+    // Pass the event to be handled by the next application in the chain
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
 
-    // Check if any modifier keys are pressed.
-    if (GetKeyboardState(KeyState)) {
-        if (KEYDOWN(KeyState[VK_SHIFT]) || KEYDOWN(KeyState[VK_CONTROL])
-          || KEYDOWN(KeyState[VK_MENU]) || KEYDOWN(KeyState[VK_LWIN])
-          || KEYDOWN(KeyState[VK_RWIN])) {
-            goto finish;
-        }
-    }
-
-    // If hotcorner, we check for forwards or backwards mousewheel rotation.
-    // And change the volume accordingly.
+static LRESULT HandleMouseWheelEvent(int nCode, WPARAM wParam, LPARAM lParam) {
+    MSLLHOOKSTRUCT *evt = (MSLLHOOKSTRUCT *) lParam;
     short wheelDelta = HIWORD(evt->mouseData);
-	if (PtInRect(&kTopLeftHotCorner, evt->pt)) {
+
+	if (PtInRect(&kTopLeftHotCorner, evt->pt) && !modifierKeysPressedDown()) {
 		if (wheelDelta > 0) {
 			SendInput(_countof(kVolumeUpInput), kVolumeUpInput, sizeof(INPUT));
 		} else {
 			SendInput(_countof(kVolumeDownInput), kVolumeDownInput, sizeof(INPUT));
 		}
-		//Prevents the mouse wheel event from being handled by the program underneath
+		// This prevents an event from being handled by the application underneath
 		return 1;
 	}
-	
-	//Switch virtual desktops with the mousewheel on the top-right hotcorner
-	if (PtInRect(&kTopRightHotCorner, evt->pt)){
+	if (PtInRect(&kTopRightHotCorner, evt->pt) && !modifierKeysPressedDown()){
 		if (wheelDelta > 0) {
 			SendInput(_countof(kDesktopLeftInput), kDesktopLeftInput, sizeof(INPUT));
 		} else {
@@ -99,26 +92,38 @@ static LRESULT CALLBACK MouseHookCallback(int nCode, WPARAM wParam, LPARAM lPara
 		}
 		return 1;
 	}
-	
-
-    finish:
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-// Get coordinates for corners other than top-left
-void UpdateCorners(){
-    int screenResX;
-    screenResX = GetSystemMetrics(SM_CXSCREEN);
-    kTopRightHotCorner.left     =   screenResX - 20;
-	kTopRightHotCorner.top      =   -20;
-	kTopRightHotCorner.right    =   screenResX + 20;
-    kTopRightHotCorner.bottom   =   +20;
+// Mouse event handler
+static LRESULT CALLBACK MouseHookCallback(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (wParam == WM_MBUTTONUP) {
+        return HandleMiddleButtonUpEvent(nCode, wParam, lParam);
+    }
+    if (wParam == WM_MOUSEWHEEL) {
+        return HandleMouseWheelEvent(nCode, wParam, lParam);
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+// Get coordinates for corners other than top-left (main monitor)
+static void UpdateCorners() {
+    int screenResX = GetSystemMetrics(SM_CXSCREEN);
+    kTopLeftHotCorner.left      =   -CORNER_SIZE;
+	kTopLeftHotCorner.top       =   -CORNER_SIZE;
+	kTopLeftHotCorner.right     =   +CORNER_SIZE;
+    kTopLeftHotCorner.bottom    =   +CORNER_SIZE;
+    kTopRightHotCorner.left     =   screenResX - CORNER_SIZE;
+	kTopRightHotCorner.top      =   -CORNER_SIZE;
+	kTopRightHotCorner.right    =   screenResX + CORNER_SIZE;
+    kTopRightHotCorner.bottom   =   +CORNER_SIZE;
 }
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     MSG Msg;
     HHOOK MouseHook;
 
+	SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
 	SetProcessDPIAware();
 	UpdateCorners();
     
